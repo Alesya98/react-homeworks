f201ed77c94cf662f536b8c2f13925cc11d7038f
Alesya98 <Petrovskaya_alesya98@mail.ru>
2026-01-08 12:40:37 +0300

    hw-lifeCycle-02

---
+++ rrr-cycle-02/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import './App.css'
+import MyComp from './MyComp'
+
+function App() {
+
+  return (
+    <>
+  <MyComp/>
+    </>
+  )
+}
+
+export default App
