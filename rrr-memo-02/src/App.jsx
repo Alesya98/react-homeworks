d6352b9e3a931b1350cfef0b23dd041fbd579b4c
Alesya98 <Petrovskaya_alesya98@mail.ru>
2026-01-19 16:59:52 +0300

    hw-memo-02

---
+++ rrr-memo-02/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+import './App.css'
+import User from './ProfileUser'
+import TaskList from './TaskList'
+import BasketProduct from './BasketProduct'
+
+function App() {
+
+  return (
+    <>
+     <User/>
+     <TaskList/>
+     <BasketProduct/>
+    </>
+  )
+}
+
+export default App
---
+++ rrr-memo-02/src/ProfileUser.jsx
@@ -0,0 +1,46 @@
+import { useState, memo } from "react";
+
+const User = () => {
+  const [user, setUser] = useState({
+    name: "Иван",
+    age: 25,
+    isActive: true,
+  });
+
+  const changeName = () => {
+    setUser({
+      ...user,
+      name: "Алеся",
+    });
+  };
+
+  const increaseAge = () => {
+    setUser({
+        ...user,
+        age: user.age + 1
+    })
+  }
+
+  const toggleActive = () => {
+    setUser({
+        ...user,
+        isActive: !user.isActive
+    })
+  }
+
+  console.log('render User')
+  return (
+    <>
+      <h2>Профиль пользователя</h2>
+      <p>Имя: {user.name} </p>
+      <p>Возраст: {user.age} </p>
+      <p>Активен: {user.isActive ? "Активен" : "Не активен"} </p>
+
+      <button onClick={changeName}>Сменить имя</button>
+      <button onClick={increaseAge}>Увеличить возраст</button>
+      <button onClick={toggleActive}>Переключить активность</button>
+    </>
+  );
+};
+
+export default memo(User);
